933e6b1f876c1f91f8307aedd258068f5c056961
zgcwkj <30312632+zgcwkj@users.noreply.github.com>
2018-10-17 16:36:19 +0800

    Update Program.cs

--- TestPassword V1.8.1.0/TestPassword/Program.cs
+++ TestPassword V1.8.1.0/TestPassword/Program.cs
@@ -20,7 +20,6 @@ namespace zgcwkj
                 {
                     Application.EnableVisualStyles();
                     Application.SetCompatibleTextRenderingDefault(false);
-                    //if (ServiceVerificat()) Application.Run(new Main());
                     new HttpProxy().SwitchOff();
                     Application.Run(new Main());
                 }
@@ -32,96 +31,6 @@ namespace zgcwkj
                     System.Environment.Exit(1);
                 }
             }
-        }
-
-        /// <summary>
-        /// 程序服务验证
-        /// </summary>
-        /// <returns></returns>
-        private static bool ServiceVerificat()
-        {
-            //返回的状态
-            bool status = false;
-
-            try
-            {
-                new HttpProxy().SwitchOff();
-                string Serve = HttpHelp.HttpGet("http://www.zgcwkj.top/TestPassword/Serve.txt");
-                string[] Serves = Serve.Replace("\r", "").Split('\n');
-
-                #region 更新模块
-
-                if (Serves.Length >= 1)
-                {
-                    if (Serves[0] != "")
-                    {
-                        string[] Update = Serves[0].Split('|');
-                        //是否强制更新
-                        if (Convert.ToBoolean(Update[0]))
-                        {
-                            //匹配版本号进行判断
-                            int versions = Convert.ToInt32(Application.ProductVersion.Replace(".", ""));
-                            if (versions < Convert.ToInt32(Update[1]))
-                            {
-                                //提示更新内容
-                                MessageBox.Show(Update[2]);
-                                //通过浏览器跳转
-                                try { System.Diagnostics.Process.Start(Update[3]); } catch { }
-                                //直接终止程序运行
-                                return false;
-                            }
-                        }
-                    }
-                }
-
-                #endregion 更新模块
-
-                #region 程序状态
-
-                if (Serves.Length >= 2)
-                {
-                    if (Serves[1] != "")
-                    {
-                        //程序状态
-                        if (Convert.ToBoolean(Serves[1])) { status = true; }
-                    }
-                }
-
-                #endregion 程序状态
-
-                #region 提示内容
-
-                if (Serves.Length >= 3)
-                {
-                    if (Serves[2] != "")
-                    {
-                        //提示消息内容
-                        MessageBox.Show(Serves[2]);
-                    }
-                }
-
-                #endregion 提示内容
-
-                #region 浏览器跳转
-
-                if (Serves.Length >= 4)
-                {
-                    if (Serves[3] != "")
-                    {
-                        //通过浏览器跳转
-                        try { System.Diagnostics.Process.Start(Serves[3]); } catch { }
-                    }
-                }
-
-                #endregion 浏览器跳转
-            }
-            catch (Exception e)
-            {
-                string str = e.Message;
-                MessageBox.Show("请检查网络状态");
-                status = false;
-            }
-            return status;
         }
     }
 }
