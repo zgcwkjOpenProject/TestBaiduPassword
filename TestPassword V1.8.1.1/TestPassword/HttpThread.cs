ce391e95ff174fafcb08cf5e01098cb74a54eabe
zgcwkj <zgcwkj@139.com>
2018-11-01 10:35:48 +0800

    更新版本到 V1.8.1.1

---
+++ TestPassword V1.8.1.1/TestPassword/HttpThread.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace zgcwkj
+{
+    public class HttpThread
+    {
+        private Main myMain;//主窗体
+        private int StartValue = 0;//基值
+        private int NumberValue = 0;//剩余次数
+        private Timer timer;//线程
+        private string ThreadID;//线程ID
+
+        /// <summary>
+        /// http请求线程类
+        /// </summary>
+        /// <param name="main">主窗体</param>
+        /// <param name="name">线程名称</param>
+        /// <param name="start">开始值</param>
+        /// <param name="number">结束值</param>
+        /// <param name="interval">线程速度</param>
+        public HttpThread(Main main, string name, int start, int number, int interval)
+        {
+            myMain = main;
+            StartValue = start;
+            NumberValue = number;
+            ThreadID = StaticClass.file + "/" + name;
+            timer = new Timer(interval);
+        }
+
+        /// <summary>
+        /// 启动线程
+        /// </summary>
+        /// <returns></returns>
+        public HttpThread Start()
+        {
+            #region 载入历史参数
+
+            try
+            {
+                if (File.Exists(ThreadID + ".ini"))
+                {
+                    string _start = File.ReadAllText(ThreadID + ".ini");
+                    if (_start != "") StartValue = Convert.ToInt32(_start);
+                }
+            }
+            catch { }
+
+            #endregion 载入历史参数
+
+            timer.Elapsed += new ElapsedEventHandler(_Timer);
+            timer.Start();
+            return this;
+        }
+
+        private void _Timer(object sender, ElapsedEventArgs e)
+        {
+            Timer timer = sender as Timer;
+            if (NumberValue < StartValue || StaticClass.status)
+            {
+                #region 保存历史参数
+
+                try
+                {
+                    File.WriteAllText(ThreadID + ".ini", StartValue.ToString());
+                }
+                catch { }
+
+                #endregion 保存历史参数
+
+                timer.Stop();
+            }
+            else
+            {
+                string password = Hash.HashTo4Str(StartValue);
+                HttpTest httptest = new HttpTest(StaticClass.url);
+                myMain.ModifyStr(httptest.Post(password), password);
+                StartValue = StartValue + 1;
+            }
+        }
+    }
+}
---
+++ TestPassword V1.8.1.1/TestPassword/Proxy.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace zgcwkj
+{
+    partial class Proxy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Proxy));
+            this.txt_1 = new System.Windows.Forms.Label();
+            this.txt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_1
+            // 
+            this.txt_1.Font = new System.Drawing.Font("宋体", 13F);
+            this.txt_1.Location = new System.Drawing.Point(14, 57);
+            this.txt_1.Name = "txt_1";
+            this.txt_1.Size = new System.Drawing.Size(183, 23);
+            this.txt_1.TabIndex = 0;
+            this.txt_1.Text = "000.000.000.000:0000";
+            // 
+            // txt
+            // 
+            this.txt.AutoSize = true;
+            this.txt.Font = new System.Drawing.Font("宋体", 14F);
+            this.txt.Location = new System.Drawing.Point(12, 28);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(143, 19);
+            this.txt.TabIndex = 1;
+            this.txt.Text = "当前尝试的IP：";
+            // 
+            // Proxy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 111);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.txt_1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(220, 150);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(220, 150);
+            this.Name = "Proxy";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "自动代理服务";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txt_1;
+        private System.Windows.Forms.Label txt;
+    }
+}
